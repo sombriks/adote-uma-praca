bf0133ec0c0daefec267a29002b40d855c40c9e0
sombriks <sombriks@gmail.com>
2015-08-06 17:05:41 -0300

    tomando forma. parte das pastas é pro lado do cliente: - bower_components - app - public (vou remover essa em breve)
    outras são pro servidor:
    - node_modules
    - model
    - views
    - routes
    
    e a conf vou guardar. colocar um esquema de banco lá. ou mesmo mandar
    embora. vou ver.

--- C-code/plaza-care/app.js
+++ C-code/plaza-care/app.js
@@ -15,7 +15,6 @@ var knex = require("knex")({
 });
 var model = require('./model')(knex);
 var routes = require('./routes')(model);
-var user = require('./routes/user')(model);
 var http = require('http');
 var path = require('path');
 
@@ -25,13 +24,14 @@ var app = express();
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
-app.use(express.favicon());
+//app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(app.router);
-app.use(express.static(path.join(__dirname, 'bower_components')));
-// app.use(express.static(path.join(__dirname, 'public')));
+// pastas com arquivos estáticos
+app.use("/app", express.static(path.join(__dirname, 'app')));
+app.use("/lib", express.static(path.join(__dirname, 'bower_components')));
 
 // development only
 if ('development' == app.get('env')) {
@@ -39,7 +39,6 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', routes.index);
-app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function() {
 	console.log('Express server listening on port ' + app.get('port'));
