f2cc79cfe610ceae8907f06ba6333ab5e2b9c405
sombriks <sombriks@gmail.com>
2015-08-16 02:27:52 -0300

    tela principal uns 80% concluída

--- C-code/aup/routes/index.js
+++ C-code/aup/routes/index.js
@@ -3,14 +3,17 @@ var router = express.Router();
 
 module.exports = function(knex) {
 
-	// na hora do login
 	router.post("/login", function(req, res) {
 		var cuidador = req.body;
 		knex("cuidador").select().where({
 			idfacebook : cuidador.idfacebook
 		}).then(function(result) {
 			if (result.length) {
-				res.json(result[0]);
+				knex("cuidador").update(cuidador).where({
+					idfacebook : cuidador.idfacebook
+				}).then(function() {
+					res.json(result[0]);
+				});
 			} else {
 				knex("cuidador").insert(cuidador, "idcuidador")//
 				.then(function(retid) {
@@ -20,6 +23,6 @@ module.exports = function(knex) {
 			}
 		});
 	});
-	
+
 	return router;
 };
