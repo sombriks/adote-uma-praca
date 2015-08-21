ee068c639d9d72a82bfc15aac61a09d9307eea81
sombriks <sombriks@gmail.com>
2015-08-21 13:06:43 -0300

    quase salvando uma praça

--- C-code/aup/public/javascripts/states/detalhepraca.js
+++ C-code/aup/public/javascripts/states/detalhepraca.js
@@ -3,16 +3,28 @@ angular.module("aup").config(function($stateProvider,$urlRouterProvider){
         name:"detalhepraca",
         url:"/detalhepraca",
         templateUrl:"views/detalhepraca.html",
-        controller:function($scope,$location) {
+        controller:function($scope,$location,Plazas) {
             $scope.plaza = {
                 nomeplaza:"",
                 lat:-3.7426492,
                 lng:-38.5374927,
                 descricaoplaza:"",
+                message:"Nova praça",
                 zoom:25
             };
-            $scope.markers=[];
+            $scope.markers=[$scope.plaza];
             //https://github.com/tombatossals/angular-leaflet-directive/tree/master/examples
+            $scope.$on("leafletDirectiveMap.click", function(event, args){
+                var leafEvent = args.leafletEvent;
+                $scope.plaza.lat=leafEvent.latlng.lat;
+                $scope.plaza.lng=leafEvent.latlng.lng;
+                $scope.plaza.message=$scope.plaza.nomeplaza;
+            });
+            $scope.salvar=function(){
+                Plazas.salvar($scope.plaza).then(function(res){
+                    
+                });
+            };
         }
     });
 });
